abfbf9778bf7c0dda104aa4fc2b1fef0585323ea
Johnny Moya <profjmoya@gmail.com>
2021-10-14 21:24:00 -0500

    commit tree c20a913e901a1296ef66569484791da133237993

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+    return arrayOfDrivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (arrayOfDrivers) {
+    return arrayOfDrivers.slice(-2);
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplyFactor) {
+    return function(fare) {
+        return multiplyFactor * fare; 
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, selectedDrivers) {
+    if(selectedDrivers === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arrayOfDrivers);
+    }
+    else {
+        return returnLastTwoDrivers(arrayOfDrivers);
+    }
+}
